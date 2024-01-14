4222e587ca36a818dacc87ca819a0afaa5ccb801
Miguel Passinhas <48985218+passas@users.noreply.github.com>
2024-01-14 01:37:03 +0000

    code

---
+++ Agenda/1/Evento.java
@@ -0,0 +1,138 @@
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+public class Evento
+{
+	//Class
+	private static LocalDateTime calculaFim (LocalDateTime inicio, long duracao)
+	{
+		LocalDateTime fim;
+
+		fim = LocalDateTime.from (inicio);
+		fim.plusMinutes (duracao);
+
+		return fim;
+	}
+
+	private static long calculaDuracao (LocalDateTime inicio, LocalDateTime fim)
+	{
+		long duracao;
+
+		duracao = 0;
+		try {
+			duracao = Duration.between (inicio, fim).toMinutes();
+		} catch (ArithmeticException e) {
+			//overflow
+			//1. Keep 0;
+			//2. Throw EventoHoldingMinutesException (inicio, fim).
+		}
+
+		return duracao;
+	}
+
+	//Instance
+	protected LocalDateTime inicio;
+	protected LocalDateTime fim;
+	protected long duracao; //minutos
+	protected String nome;
+
+	public Evento (LocalDateTime inicio, long duracao, String nome)
+	{
+		this.inicio = LocalDateTime.from (inicio);
+		this.duracao = duracao;
+		this.nome = nome;
+
+		this.fim = Evento.calculaFim (inicio, duracao);
+	}
+
+	public Evento (LocalDateTime inicio, LocalDateTime fim, String nome)
+	{
+		this.inicio = LocalDateTime.from (inicio);
+		this.fim = LocalDateTime.from (fim);
+		this.nome = nome;
+
+		this.duracao = Evento.calculaDuracao (inicio, fim);
+	}
+
+	//Setters
+	public void setInicio (LocalDateTime inicio)
+	{
+		this.inicio = LocalDateTime.from (inicio);
+
+		//collateral
+		this.duracao = Evento.calculaDuracao (inicio, this.fim);
+	}
+
+	public void setFim (LocalDateTime fim)
+	{
+		this.fim = LocalDateTime.from (fim);
+
+		//collateral
+		this.duracao = Evento.calculaDuracao (this.inicio, fim);
+	}
+
+	public void setDuracao (long duracao)
+	{
+		this.duracao = duracao;
+
+		//collateral
+		this.fim = Evento.calculaFim (this.inicio, duracao);
+	}
+
+	public void setNome (String nome)
+	{
+		this.nome = nome;
+	}
+
+	//Getters
+	public LocalDateTime getInicio ()
+	{
+		return LocalDateTime.from (this.inicio);
+	}
+
+	public LocalDateTime getFim ()
+	{
+		return LocalDateTime.from (this.fim);
+	}
+
+	public long getDuracao ()
+	{
+		return this.duracao;
+	}
+
+	public String getNome ()
+	{
+		return this.nome;
+	}
+
+	//Object
+	@Override
+	public String toString ()
+	{
+		String s;
+
+		s = "";
+		s += inicio + " " + fim + " " + duracao + " " + nome;
+
+		return s;
+	}
+
+	//main
+	public static void main (String args[])
+	{
+		LocalDateTime inicio, fim;
+		String nome;
+		Evento e;
+
+		//Teste 00:30 - 00:00
+		inicio = LocalDateTime.of (2024, 01, 20, 00, 00);
+		fim = LocalDateTime.of (2024, 01, 20, 00, 30);
+		nome = "Duration between 00:00 a.m. and 00:30 p.m.)";
+		e = new Evento (inicio, fim, nome);
+		System.out.println (e);
+		//Teste ok
+
+
+	}
+
+}
